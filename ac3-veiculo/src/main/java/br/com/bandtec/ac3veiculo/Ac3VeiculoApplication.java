6418a58f79b87e33b19afef57d69464653470429
CESAR DE MOURA ALMEIDA <cesar.almeida@bandtec.com.br>
2021-05-27 15:14:14 -0300

    Acertos

--- ac3-veiculo/src/main/java/br/com/bandtec/ac3veiculo/Ac3VeiculoApplication.java
+++ ac3-veiculo/src/main/java/br/com/bandtec/ac3veiculo/Ac3VeiculoApplication.java
@@ -2,7 +2,9 @@ package br.com.bandtec.ac3veiculo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class Ac3VeiculoApplication {
 
--- ac3-veiculo/src/main/java/br/com/bandtec/ac3veiculo/servico/AgendamentoService.java
+++ ac3-veiculo/src/main/java/br/com/bandtec/ac3veiculo/servico/AgendamentoService.java
@@ -1,13 +1,18 @@
 package br.com.bandtec.ac3veiculo.servico;
 
+import br.com.bandtec.ac3veiculo.repositorio.VeiculoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AgendamentoService {
+    @Autowired
+    private VeiculoRepository repository;
 
-    @Scheduled(fixedRate = 3000)
+    @Scheduled(initialDelay = 1000, fixedRate = 3000)
     public void filosofar() {
+//        System.out.println(repository.findAll());
         System.out.println("Não desista até conseguir 8");
     }
 }
