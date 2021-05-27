7de07f355bd186cc1d1d09e766b6688211c21ec6
CESAR DE MOURA ALMEIDA <cesar.almeida@bandtec.com.br>
2021-05-26 22:13:32 -0300

    Data sql

--- ac3-veiculo/src/main/resources/data.sql
+++ ac3-veiculo/src/main/resources/data.sql
@@ -2,4 +2,6 @@ insert into garagem values
 (1, true, 'César de Moura Almeida'), (2, false , 'Bruno de Moura Almeida'), (3, false , 'Manoel Ferreira Matos');
 
 insert into veiculo values
-('8sHeAA73HUCEv7241', 2019, 'GM - Chevrolet', 'SONIC HB LT 1.6 16V FlexPower 5p Aut.', 129.8, 4, 23.7, 1);
+('8sHeAA73HUCEv7241', 2019, 'GM - Chevrolet', 'SONIC HB LT 1.6 16V FlexPower 5p Aut.', 129.8, 4, 23.7, 1),
+('5BgAApz9K1tRm7024', 2015, 'Troller', 'T-4 DESERT STORM 4x4 3.0 TB Int Diesel', 627.8, 4, 55.4, 2),
+('15ApW7V125Gb69717', 2009, 'GM - Chevrolet', 'Lumina', 928.1, 4, 77.5, 2);
